d7ff6b14a8232dea075d6c845244b0dcc7eb33fa
Francisndungu2 <francianmbugua@gmail.com>
2020-11-14 22:45:49 +0300

    add pipe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AboutComponent } from './about/about.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { ProfileComponent } from './profile/profile.component';
+import { ProfileFormComponent } from './profile-form/profile-form.component';
+import { DatePipe } from './date.pipe';
+import { MousehoverDirective } from './mousehover.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    NavbarComponent,
+    ProfileComponent,
+    ProfileFormComponent,
+    DatePipe,
+    MousehoverDirective
   ],
   imports: [
     BrowserModule,
