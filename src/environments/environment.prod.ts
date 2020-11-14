876f150ee8ef709393509ee7b41cb76ea2dd266b
Francisndungu2 <francianmbugua@gmail.com>
2020-11-14 23:54:36 +0300

    addapikey

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: false,
+  Apikey:"4409e2d1b2a827b684d3b8ab5d1f998527ce89d4",
+  ApiUrl:'https://api.github.com/users/'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,14 +3,9 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  Apikey:"4409e2d1b2a827b684d3b8ab5d1f998527ce89d4",
+  ApiUrl:'https://api.github.com/users/'
 };
 
-/*
- * For easier debugging in development mode, you can import the following file
- * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
- *
- * This import should be commented out in production mode because it will have a negative impact
- * on performance if an error is thrown.
- */
-// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
+
