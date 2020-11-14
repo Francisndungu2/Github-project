fe5374cc53fec900ca62f7ee806b3b72e7104c7e
Francisndungu2 <francianmbugua@gmail.com>
2020-11-14 23:59:22 +0300

    addlinks

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ProfileServiceService } from '../profile-service.service'
+import { Profile } from '../profile';
+import { Repo } from '../repo';
 
 @Component({
   selector: 'app-profile',
@@ -7,9 +10,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfileComponent implements OnInit {
 
-  constructor() { }
+  profile: Profile
+  repos: Repo;
 
-  ngOnInit(): void {
+  constructor(private profileService: ProfileServiceService) {
   }
 
+  performsearch(user) {
+    this.profileService.profileRequest(user).then((res) => {
+      this.profile = this.profileService.profile;
+    }, err => {
+      console.log(err);
+    })
+    this.profileService.displayRepos(user).then((res) => {
+      this.repos = this.profileService.repo;
+      console.log(this.repos);
+    }, error => {
+      console.log(error);
+    })
+
+
+
+  }
+
+  ngOnInit(): void {
+    this.performsearch('Francisndungu2');
+  }
 }
